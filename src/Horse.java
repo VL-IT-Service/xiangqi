4932a4a64083f8b634d33a8ce109ab9639f4051c
Jens van Laak <jens@vl-it.de>
2022-02-05 15:05:10 +0100

    Horse movement

--- src/Horse.java
+++ src/Horse.java
@@ -5,6 +5,25 @@ public class Horse extends GameToken{
 
     @Override
     public void checkMoveInDetail(int moveStartRow, int moveStartCol, int moveEndRow, int moveEndCol) throws MoveNotPossibleException {
+        // check distance
+        int dr = moveEndRow-moveStartRow;
+        int dc = moveEndCol-moveStartCol;
+        if (! ((Math.abs(dr) == 2 && Math.abs(dc) == 1) || ( Math.abs(dr) == 1 && Math.abs(dc) == 2 ) ) ){
+            throw new MoveNotPossibleException("Horse can only move one field straight and one diagonal.");
+        }
+        // Horse goes up or down
+        if (Math.abs(dr) == 2){
+            if (board.getField(moveStartRow + Integer.signum(dr),moveStartCol) != null){
+                throw new MoveNotPossibleException("The Horse needs a free field next to it in direction of movement.");
+            }
+        }
+        // Horse goes left or right
+        if (Math.abs(dc) == 2){
+            if (board.getField(moveStartRow ,moveStartCol+ Integer.signum(dc)) != null){
+                throw new MoveNotPossibleException("The Horse needs a free field next to it in direction of movement.");
+            }
+        }
+
 
     }
 
