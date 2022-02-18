3fd6140d36b055974f874a99af74cf87feac7065
jvl <jvl@work.de>
2022-02-18 10:54:17 +0100

    Tests added

--- src/test/java/GameBoardTest.java
+++ src/test/java/GameBoardTest.java
@@ -19,9 +19,10 @@ class GameBoardTest {
     @Test
     void exportBoard() {
         GameBoard gameBoard = new GameBoard();
-        gameBoard.importBoard("rheagaehr/9/1c5c1/s1s1s1s1s/9/9/S1S1S1S1S/1C5C1/9/RHEAGAEHR");
+        String fen = "rheagaehr/9/1c5c1/s1s1s1s1s/9/9/S1S1S1S1S/1C5C1/9/RHEAGAEHR";
+        gameBoard.importBoard(fen);
         assertAll(
-                () -> assertEquals("rheagaehr/9/1c5c1/s1s1s1s1s/9/9/S1S1S1S1S/1C5C1/9/RHEAGAEHR", gameBoard.exportBoard() )
+                () -> assertEquals(fen, gameBoard.exportBoard() )
         );
     }
 
