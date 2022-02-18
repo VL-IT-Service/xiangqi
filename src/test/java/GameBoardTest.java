8339cad1d6d0f5598475398042f643cc0d98862e
jvl <jvl@work.de>
2022-02-18 08:46:09 +0100

    Tests added

--- src/test/java/GameBoardTest.java
+++ src/test/java/GameBoardTest.java
@@ -9,13 +9,20 @@ class GameBoardTest {
         GameBoard gameBoard = new GameBoard();
         gameBoard.importBoard("rheagaehr/9/1c5c1/s1s1s1s1s/9/9/S1S1S1S1S/1C5C1/9/RHEAGAEHR");
         assertAll(
-                () -> assertEquals("rheagaehr/9/1c5c1/s1s1s1s1s/9/9/S1S1S1S1S/1C5C1/9/RHEAGAEHR", gameBoard.exportBoard() )
+                () -> assertEquals(null, gameBoard.getField(1,1)),
+                () -> assertEquals(null, gameBoard.getField(8,3)) ,
+                () -> assertNotEquals(null, gameBoard.getField(0,3))
         );
 
     }
 
     @Test
     void exportBoard() {
+        GameBoard gameBoard = new GameBoard();
+        gameBoard.importBoard("rheagaehr/9/1c5c1/s1s1s1s1s/9/9/S1S1S1S1S/1C5C1/9/RHEAGAEHR");
+        assertAll(
+                () -> assertEquals("rheagaehr/9/1c5c1/s1s1s1s1s/9/9/S1S1S1S1S/1C5C1/9/RHEAGAEHR", gameBoard.exportBoard() )
+        );
     }
 
     @Test
