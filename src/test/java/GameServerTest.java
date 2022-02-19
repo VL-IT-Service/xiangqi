df3c13927fcee2d969fb75f94df740634dedbbef
Jens van Laak <jens@vl-it.de>
2022-02-19 09:43:54 +0100

    Tests GameServerTest.checkMoveFormat

--- src/test/java/GameServerTest.java
+++ src/test/java/GameServerTest.java
@@ -4,17 +4,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GameServerTest {
 
-    @Test
-    void getBoard() {
 
-    }
 
     @Test
     void checkMoveFormat() {
-
+        GameServer gs = new GameServer();
+        assertTrue(gs.checkMoveFormat("e1-e9"));
+        assertFalse(gs.checkMoveFormat("A3-F7"));
+        assertFalse(gs.checkMoveFormat("zuludsufsdadfiausf"));
+        assertFalse(gs.checkMoveFormat("a3:a2"));
+        assertFalse(gs.checkMoveFormat("ag:a2"));
     }
 
     @Test
     void makeMove() {
+
+
+
+
     }
 }
